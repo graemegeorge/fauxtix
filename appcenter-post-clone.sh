d12f34532a7a97480d8378b58eac3dc798b9ece1
graeme <graeme@motix.io>
2018-05-22 22:00:55 +0100

    Added post-clone shell script

---
+++ appcenter-post-clone.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+set -e
+brew uninstall node@6
+NODE_VERSION="8.8.0"
+curl "https://nodejs.org/dist/v${NODE_VERSION}/node-v${NODE_VERSION}.pkg" > "$HOME/Downloads/node-installer.pkg"
+sudo installer -store -pkg "$HOME/Downloads/node-installer.pkg" -target "/"
+#npm install exp exptool -g
+#exp path
